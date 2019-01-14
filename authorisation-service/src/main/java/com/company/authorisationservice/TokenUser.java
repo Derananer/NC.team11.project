dc07d07982e13676d2eb1332ab8e39daf0082cfd
Derananer <10ilay1998@gmail.com>
2019-01-14 18:21:27 +0400

    update authentication-service(add Token)

---
+++ authorisation-service/src/main/java/com/company/authorisationservice/TokenUser.java
@@ -0,0 +1,31 @@
+package com.company.authorisationservice;
+
+public class TokenUser {
+    private final String id;
+    private final String userName;
+    private final String departmentId;
+    private final String permission;
+
+    public TokenUser(String id, String userName, String departmentId, String permission) {
+        this.id = id;
+        this.userName = userName;
+        this.departmentId = departmentId;
+        this.permission = permission;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
